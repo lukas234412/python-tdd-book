5ed8f1f86efdec81051da30a5438f3bb28b1ebcc
lukas234412 <lukas234412@gmail.com>
2020-10-06 22:18:33 +0200

    filling in missing comments in functional_tests.py

--- functional_tests.py
+++ functional_tests.py
@@ -22,3 +22,22 @@ class NewVisitorTest(unittest.TestCase):
 
 if __name__ == '__main__':
 	unittest.main()
+	
+# She types "Buy peacock feathers" into a text box (Edith's 
+# hobby is tying fly-fishing lures)
+	
+# When she hits enter, the page updates, and now the page lists 
+# "1: Buy peacock feathers" as an item in a to-do list
+	
+# There is still a text box inviting her to add another item. She
+# enters "Use peacock feathers to make a fly" (Edith is very metodical)
+	
+# The page updates again, and shows both items on her list
+	
+# Edith wonders wheather the site will remember her list. Then she sees
+# that the site has generated a unique URL for her -- there is some 
+# explanatory text to that effect.
+
+# She visits that URL - her to-do list is still there.
+
+# Satisfied, she goes back to sleep
