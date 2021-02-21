c5d4440a89200782be9abbf12e423b7b58a27e8c
lukas234412 <lukas234412@gmail.com>
2021-02-21 10:06:26 +0100

    ch16.3 smoke test to see how QUnit works

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -59,7 +59,7 @@ class ItemValidationTest(FunctionalTest):
 		self.get_item_input_box().send_keys('Buy wellies')
 		self.get_item_input_box().send_keys(Keys.ENTER)
 		
-		# She sees a helpfull error message
+		# She sees a helpful error message
 		self.wait_for(lambda: self.assertEqual(
 			self.get_error_element().text,
 			"You've already got this in your list"
