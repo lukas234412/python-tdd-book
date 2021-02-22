3c16d2cd3a8b317852a85b15e319add290d54809
lukas234412 <lukas234412@gmail.com>
2021-02-22 21:06:47 +0100

    ch16.5 tests pass with console.log();

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,3 +1,8 @@
-$('input[name="text"]').on('keypress', function () {
-    $('.has-error').hide();
-});
+var initialize = function () {
+    console.log('initialize called');
+    $('input[name="text"]').on('keypress', function () {
+        console.log('in keypress handler');
+        $('.has-error').hide();
+    });
+};
+console.log('list.js loaded');
