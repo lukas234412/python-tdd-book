871859b14ad670e1d10444104184c5db123c79c7
lukas234412 <lukas234412@gmail.com>
2021-04-11 20:09:08 +0200

    test and migration for user model ch16l028-1

--- superlists/settings.py
+++ superlists/settings.py
@@ -15,32 +15,31 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 if 'DJANGO_DEBUG_FALSE' in os.environ:
-	DEBUG = False
-	SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
-	ALLOWED_HOSTS = [os.environ['SITENAME']]
+    DEBUG = False
+    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+    ALLOWED_HOSTS = [os.environ['SITENAME']]
 else:
-	# SECURITY WARNING: keep the secret key used in production secret!
-	SECRET_KEY = 'jc$n)+6o(ve90k%%4)r3e&jg*05t!ev6qjiz8nl%+&-y$s)4@('
-	# SECURITY WARNING: don't run with debug turned on in production!
-	DEBUG = True
-	ALLOWED_HOSTS = []
-
+    # SECURITY WARNING: keep the secret key used in production secret!
+    SECRET_KEY = 'jc$n)+6o(ve90k%%4)r3e&jg*05t!ev6qjiz8nl%+&-y$s)4@('
+    # SECURITY WARNING: don't run with debug turned on in production!
+    DEBUG = True
+    ALLOWED_HOSTS = []
 
 # Application definition
 
 INSTALLED_APPS = [
-    #'django.contrib.admin',
+    # 'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'lists',
+    'lists',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -73,7 +72,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'superlists.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
@@ -83,7 +81,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -103,7 +100,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
@@ -117,13 +113,11 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
 
 # Logging of exceptions
 
@@ -143,3 +137,5 @@ LOGGING = {
     },
     'root': {'level': 'INFO'},
 }
+
+AUTH_USER_MODEL = 'accounts.User'
